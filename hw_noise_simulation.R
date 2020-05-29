800fc80ba805074982c7ccb4221daa7fdc3cbb69
Percimorphsim <manifoldmorphism@gmail.com>
2020-05-28 21:29:36 -0400

    modularized stoch_sim

--- hw_noise_simulation.R
+++ hw_noise_simulation.R
@@ -1,38 +1,32 @@
-source('hw_util.R')
-source('hw_simulation.R')
+
+build_distance_matrices_noise<-function(hw_noise_data){
+  # URF noise
+  g_noise=randomForest(hw_noise_data, keep.forest=FALSE, proximity=TRUE)
+  W_urf_noise = g_noise$proximity
+  D_urf_noise = (1 - W_urf_noise)
+  # isomap distance matrix, with noise
+  D_noise=as.matrix(dist(hw_noise_data))
+  # pick k to be the smallest number that does not give a seg-error
+  iso_d_noise = isomapdist(D_noise, k=9)
+  D_iso_noise = as.matrix(iso_d_noise)
+  return(list('D_urf_noise'=D_urf_noise, 'D_iso_noise'=D_iso_noise ))
+}
 
 
-# Hardy-Weinberg Curve with noise
-noise_dim = 5
-v = 10
-
-noise=generate_high_dim_gaussian_noise(num_of_points, noise_dim, v)
-hw_noise_data=cbind(hw_data, noise)
-
-# URF noise
-g_noise=randomForest(hw_noise_data, keep.forest=FALSE, proximity=TRUE)
-W_rf_noise = g_noise$proximity
-D_rf_noise = (1 - W_rf_noise)
-# isomap distance matrix, with noise
-D_noise=as.matrix(dist(hw_noise_data))
-# pick k to be the smallest number that does not give a seg-error
-iso_d_noise = isomapdist(D_noise, k=9)
-D_iso_noise = as.matrix(iso_d_noise)
-
-
-# 1) the rank of "rank one point" in Isomap wrt the sp_pt (noise)
-iso_dist_rk1_sppt_noise = D_iso_noise[sp_pt, rank_one_pt_geo]
-iso_sort_noise = sort(D_iso_noise[sp_pt,])
-R_iso_noise = match(iso_dist_rk1_sppt_noise, iso_sort_noise) - 1
-
-
-# 2) the rank of "rank one point" in URF wrt the sp_pt (noise)
-# Urf distance from special point to the true rank one point
-urf_dist_rk1_sppt_noise = D_rf_noise[sp_pt, rank_one_pt_geo]
-rf_sort_noise = sort(D_rf_noise[sp_pt,])
-R_urf_noise = match(urf_dist_rk1_sppt_noise, rf_sort_noise) - 1
-
-rank_list = c(R_iso, R_urf, R_urf_noise, R_iso_noise)
-print(c('R_iso', 'R_urf', 'R_urf_noise', 'R_iso_noise'))
-
-print(rank_list)
+# # 1) the rank of "rank one point" in Isomap wrt the sp_pt (noise)
+# iso_dist_rk1_sppt_noise = D_iso_noise[sp_pt, rank_one_pt_geo]
+# iso_sort_noise = sort(D_iso_noise[sp_pt,])
+# R_iso_noise = match(iso_dist_rk1_sppt_noise, iso_sort_noise) - 1
+# 
+# 
+# # 2) the rank of "rank one point" in URF wrt the sp_pt (noise)
+# # Urf distance from special point to the true rank one point
+# urf_dist_rk1_sppt_noise = D_rf_noise[sp_pt, rank_one_pt_geo]
+# rf_sort_noise = sort(D_rf_noise[sp_pt,])
+# R_urf_noise = match(urf_dist_rk1_sppt_noise, rf_sort_noise) - 1
+# 
+# rank_list = c(R_iso, R_urf, R_urf_noise, R_iso_noise)
+# print(c('R_iso', 'R_urf', 'R_urf_noise', 'R_iso_noise'))
+# 
+# 
+# print(rank_list)
---
+++ stochastic_inequality_simulation.R
@@ -0,0 +1,42 @@
+source('hw_simulation.R')
+source('hw_util.R')
+source('hw_noise_simulation.R')
+
+# Hardy-Weinberg Curve
+num_of_points=1000
+hw_data_object=hw_data(num_of_points)
+hw_data=hw_data_object[[1]]
+t=hw_data_object[[2]]
+
+# noise parameter
+noise_dim = 5
+v = 10
+#generate noise and concatenate onto HW
+noise=generate_high_dim_gaussian_noise(num_of_points, noise_dim, v)
+hw_noise_data=cbind(hw_data, noise)
+
+#list of X in Carey's Conjecture, for stochastic
+special_point_list = c(100:300)
+
+distance_matrices = build_distance_matrices(t, num_of_points)
+D_geo = distance_matrices$D_geo
+D_urf = distance_matrices$D_urf
+D_iso = distance_matrices$D_iso
+
+distance_matrices_noise = build_distance_matrices_noise(hw_noise_data)
+D_urf_noise = distance_matrices_noise$D_urf_noise
+D_iso_noise = distance_matrices_noise$D_iso_noise
+
+rank_list_df <- data.frame(matrix(ncol = 4, nrow = 0))
+colnames(rank_list_df)<- c('R_iso', 'R_urf', 'R_urf_noise', 'R_iso_noise')
+for (sp_pt in special_point_list){
+  X_star = get_true_X_star(D_geo, sp_pt)
+  R_iso = get_R_iso(D_iso, X_star, sp_pt)
+  R_urf = get_R_urf(D_urf, X_star, sp_pt)
+  
+  R_iso_noise = get_R_iso(D_iso_noise, X_star, sp_pt)
+  R_urf_noise = get_R_urf(D_urf_noise, X_star, sp_pt)
+  
+  rank_list_df[nrow(rank_list_df) + 1,] = c(R_iso, R_urf, R_urf_noise, R_iso_noise)
+}
+
